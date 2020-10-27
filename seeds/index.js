5a84ee0a0164dc6e0dd38656013dc4594f005537
Bean Bean <hangolethanh@gmail.com>
2020-10-27 12:40:10 +0200

    basic authorization

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20 + 10);
         const camp = new Campground ({
+            author: '5f97091a8a82d4289f8a993f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)}, ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251/',
